88446363dc18f06ee413d0f8a1fe449e4aec532e
Fathir Yudhistira <fathiryudh03@gmail.com>
2026-09-12 02:16:12 +0800

    Added inventory cannot exceed 500 quantity logic

--- auditor.py
+++ auditor.py
@@ -15,8 +15,15 @@ while True:
         continue
 
     quantity = int(user_input)
+
+    if inventory + quantity > 500:
+        print("Inventory limit exceeded. Overstock!")
+        rejected_entries += 1
+        break
+
     inventory += quantity
+
     print("Current inventory count: ", inventory)
 
 print("Final inventory count: ", inventory)
-
+print("Number of rejected entries: ", rejected_entries)
